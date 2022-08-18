9dfb139a6966639128f9bd3e5641cc8cc236899b
Bjorn Jakobsen <106143706+Ukjeent@users.noreply.github.com>
2022-08-18 22:18:33 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,34 +2,43 @@
 
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const startOverlay = document.getElementById('overlay');
+const resetBtn = document.querySelector('.btn__reset');
+const hearts = document.getElementsByClassName('tries');
+const liveHeartString = '<img src="images/liveHeart.png" height="35px" width="30px"></img>';
+const lostHeartString = '<img src="images/lostHeart.png" height="35px" width="30px"></img>';
+
 let missed = 0;
+// Store the letters found in a string.
+let letterFound = '';
 
 // Removes the start overlay when a player click the start button
 document.addEventListener('click', (e) => {
     const buttonClick = e.target;
-    const startOverlay = document.getElementById('overlay');
     if (buttonClick.className === 'btn__reset') {
-        overlay.remove();
+        overlay.style.display = 'none';
     } 
 });
 
 // Array with phrases
 const phrases = [
-    'Stand up and catch that ball',
-    'Somebody help me',
-    'No problem',
-    'Look at that pumpkin',
-    'Come on down',
-    'What a big eater he is',
-    'Shape up or ship out',
-    'What a big dog'
+    'test phrase'
+    // 'Stand up and catch that ball',
+    // 'Somebody help me',
+    // 'No problem',
+    // 'Look at that pumpkin',
+    // 'Come on down',
+    // 'What a big eater he is',
+    // 'Shape up or ship out',
+    // 'What a big dog'
 ];
 
 
 // gets a random phrase from an array and stores the characters in a new array
 function getRandomPhraseAsArray(arr){
     const randomNumber = Math.floor(Math.random() * arr.length);
-    const randomPhrase = arr[randomNumber];
+    let randomPhrase = arr[randomNumber];
+    randomPhrase = randomPhrase.toLowerCase();
     const charactersInPhrase = randomPhrase.split('');
     return charactersInPhrase;
 }
@@ -54,3 +63,89 @@ function addPhraseToDisplay(arr) {
 const phraseArray = getRandomPhraseAsArray(phrases);
 // Calls the funtion that creates li elements and append to the site. 
 addPhraseToDisplay(phraseArray);
+
+
+
+function checkLetter(inputValue) {
+    const letters = document.getElementsByClassName('letter');
+    let foundLetters;
+    for (let i = 0; i < letters.length; i++) {
+     const letter = letters[i];
+     if (inputValue === letter.innerHTML){
+         letter.className = 'letter show';
+         foundLetters = inputValue;
+     }
+    }
+    return foundLetters;
+ }
+
+
+
+
+document.addEventListener('click', (e) => {
+    const click = e.target;
+    if (click.tagName === 'BUTTON' && click.parentNode.className === 'keyrow'){
+        checkLetter(e.target.innerHTML);
+        e.target.className = 'chosen';
+        e.target.disabled = true;
+            if (checkLetter(e.target.innerHTML)) {
+                letterFound += checkLetter(e.target.innerHTML)
+            } else {
+                missed++;
+                const targetHeart = 5 - missed;
+                hearts[targetHeart].innerHTML = lostHeartString;
+            }
+}
+checkWin()
+});
+
+function checkWin() {
+    const show = document.getElementsByClassName('show');
+    const letter = document.getElementsByClassName('letter');
+       if (show.length === letter.length){
+            toggleFinalOverlay('win', 'Congratulations!', 'Restart Game');
+        } else if (missed >= 5) {
+            toggleFinalOverlay('lose', 'Sorry!', 'Restart Game');
+        }
+}
+
+function toggleFinalOverlay(overlayClass, titleText, btnText){
+    const title = document.querySelector('.title');
+    overlay.style.display = '';
+    overlay.className = overlayClass;
+    title.textContent = titleText;
+    resetBtn.textContent = btnText;
+    restartGame()
+}
+
+function restartGame(){
+    const buttons = qwerty.firstElementChild.children;
+    missed = 0;
+    for (let i = 0; i < hearts.length; i++){
+        hearts[i].innerHTML = liveHeartString;
+    }
+    for (let i = 0; i < buttons.length; i++)
+        button = buttons[i];
+        if (button.disabled) {
+            button.disabled = '';
+            button.className = '';
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
