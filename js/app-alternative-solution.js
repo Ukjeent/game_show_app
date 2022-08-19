5901742f1d74bfdd272724adb01e68f06fc2b845
Bjorn Jakobsen <106143706+Ukjeent@users.noreply.github.com>
2022-08-19 22:32:57 +0200

    FIxed an error

--- js/app-alternative-solution.js
+++ js/app-alternative-solution.js
@@ -1,4 +1,4 @@
-// An alternative solution that isn't adding the letters to the li before the player guesses the letter. 
+// An alternative solution that does not add the letters to li until the player guesses the letter. 
 // That means the player can not "cheat" by opening the console and the characters will not show on hover. 
 
 const qwerty = document.getElementById('qwerty');
@@ -38,15 +38,15 @@ document.addEventListener('click', (e) => {
     const buttonClick = e.target;
     if (buttonClick.className === 'btn__reset' && buttonClick.textContent === 'Start Game') {
         overlay.style.display = 'none'; // Removes the overlay
-        getNewArrayAndAppendToSite() // Gets a new array
+        getNewArrayAndAppendToSite(); // Gets a new array
     } else if (buttonClick.className === 'btn__reset' && buttonClick.textContent === 'Continue Game') {
         overlay.style.display = 'none'; // Removes the overlay
         letterFound = ''; // Resets the string "letterFound"
-        continuePlaying() // Removes the completed phrase from the array and gets a new array so the player can continue playing. 
+        continuePlaying(); // Removes the completed phrase from the array and gets a new array so the player can continue playing. 
     } else if (buttonClick.className === 'btn__reset' && buttonClick.textContent === 'Restart Game') {
         overlay.style.display = 'none'; // Removes the overlay
         letterFound = ''; // Resets the string "letterFound"
-        restartGame() // Restarts the game. 
+        restartGame(); // Restarts the game. 
     }
 });
 
@@ -71,7 +71,7 @@ function addPhraseToDisplay(arr) {
                 letterLi.className = 'letter'; // If the string in the array includes a character, add the class "letter" to the li element with the same number. 
             } else if (phraseArray[i].trim().length === 0)  {
                 letterLi.className = 'space'; // If the string in the array includes a space, add the class "space" to the li element with the same number. 
-            };
+            }
         ulList.appendChild(letterLi); // Appends the li elements to the ul
     }
 }
@@ -89,7 +89,7 @@ function checkLetter(inputValue) {
     const liList = ulList.children;
     for (let i = 0; i < phraseArray.length; i++) {
         const li = liList[i];
-        const array = phraseArray[i]
+        const array = phraseArray[i];
      if (inputValue === array){ 
         li.innerHTML = array; // Add the character from the array to the li element if the input matches a character in the array.
         li.className = 'letter show'; // Adds the class "show" to the li element.
@@ -107,14 +107,14 @@ document.addEventListener('click', (e) => {
         e.target.className = 'chosen';
         e.target.disabled = true;
             if (checkLetter(e.target.innerHTML)) {
-                letterFound += checkLetter(e.target.innerHTML) // Adds the input letter to a variable
+                letterFound += checkLetter(e.target.innerHTML); // Adds the input letter to a variable
             } else {
                 missed++; // Adds +1 to the variable missed
                 const targetHeart = 5 - missed; 
                 hearts[targetHeart].innerHTML = lostHeartString; // Removes the last heart from the site and replaces the heart with a empty heart
             }
 }
-checkWin() // A function that checks the result. 
+checkWin(); // A function that checks the result. 
 });
 
 
@@ -126,9 +126,9 @@ function checkWin() {
        if (show.length >= 1 && show.length === letter.length && phrases.length >= 2){
             toggleFinalOverlay('win', 'Congratulations!', 'Continue Game'); // Toggles an overlay when a player got all the characters correct.
         } else if (show.length >= 1 && show.length === letter.length && phrases.length === 1) {
-            toggleFinalOverlay('win', 'Congratulations, you completed the game!', 'Restart Game') // Toggles an overlay when a player gets all phrases correct.
+            toggleFinalOverlay('win', 'Congratulations, you completed the game!', 'Restart Game'); // Toggles an overlay when a player gets all phrases correct.
         } else if (missed >= 5) {
-            toggleFinalOverlay('lose', "Sorry, that's not the correct phrase!", 'Restart Game'); // Toggles an overlay when a player has lost all five hearts. 
+            toggleFinalOverlay('lose', "Sorry, wrong guess!", 'Restart Game'); // Toggles an overlay when a player has lost all five hearts. 
         }
 }
 
@@ -145,12 +145,12 @@ function toggleFinalOverlay(overlayClass, titleText, btnText){
  // A function to continue the game and get a new array
  function continuePlaying(){
     missed = 0; // Resets the missed variable
-    resetHearts() // Resets the hearts
-    resetButtons() // Resets the buttons
-    clearLetterShowClass() // Removes the "show" class from the li elements
-    removeOldPhraseFromUl() // Removes the completed phrase from the ul
-    removeStringFromPhrases() // Removes the completed phrase from the array
-    getNewArrayAndAppendToSite() // Gets a new array, create li elements and appends to the ul. 
+    resetHearts(); // Resets the hearts
+    resetButtons(); // Resets the buttons
+    clearLetterShowClass(); // Removes the "show" class from the li elements
+    removeOldPhraseFromUl(); // Removes the completed phrase from the ul
+    removeStringFromPhrases(); // Removes the completed phrase from the array
+    getNewArrayAndAppendToSite(); // Gets a new array, create li elements and appends to the ul. 
 }
 
 // Removes a string from the array. Used when a player completes a phrase. 
@@ -162,12 +162,12 @@ function toggleFinalOverlay(overlayClass, titleText, btnText){
 // A function to restart the game
 function restartGame(){
     missed = 0; // Resets the missed variable
-    resetHearts() // Resets the hearts
-    resetButtons() // Resets the buttons
-    clearLetterShowClass() // Removes the "show" class from the li elements
-    removeOldPhraseFromUl() // Removes the completed phrase from the ul
-    addRemovedPhrasesToArray() // If the player has completed any phrases before loosing all hearts this function adds the phrases back to the array to restart the game woth all phrases. 
-    getNewArrayAndAppendToSite() // Gets a new array, create li elements and appends to the ul. 
+    resetHearts(); // Resets the hearts
+    resetButtons(); // Resets the buttons
+    clearLetterShowClass(); // Removes the "show" class from the li elements
+    removeOldPhraseFromUl(); // Removes the completed phrase from the ul
+    addRemovedPhrasesToArray(); // If the player has completed any phrases before loosing all hearts this function adds the phrases back to the array to restart the game woth all phrases. 
+    getNewArrayAndAppendToSite(); // Gets a new array, create li elements and appends to the ul. 
 }
 
 // Resets the lost hearts
@@ -205,7 +205,7 @@ function removeOldPhraseFromUl() {
 function addRemovedPhrasesToArray() {
     if (phrases.length === 1) {
         while ( removedPhrases.length > 0 ) {
-            phrases.push(removedPhrases[0])  // Adds the phrases to the phrases array
+            phrases.push(removedPhrases[0]);  // Adds the phrases to the phrases array
             removedPhrases.splice(0, 1); // Removes the phrase from the removedPhrases array
         }
     }
--- js/app.js
+++ js/app.js
@@ -36,15 +36,15 @@ document.addEventListener('click', (e) => {
     const buttonClick = e.target;
     if (buttonClick.className === 'btn__reset' && buttonClick.textContent === 'Start Game') {
         overlay.style.display = 'none'; // Removes the overlay
-        getNewArrayAndAppendToSite() // Gets a new array
+        getNewArrayAndAppendToSite(); // Gets a new array
     } else if (buttonClick.className === 'btn__reset' && buttonClick.textContent === 'Continue Game') {
         overlay.style.display = 'none'; // Removes the overlay
         letterFound = ''; // Resets the string "letterFound"
-        continuePlaying() // Removes the completed phrase from the array and gets a new array so the player can continue playing. 
+        continuePlaying(); // Removes the completed phrase from the array and gets a new array so the player can continue playing. 
     } else if (buttonClick.className === 'btn__reset' && buttonClick.textContent === 'Restart Game') {
         overlay.style.display = 'none'; // Removes the overlay
         letterFound = ''; // Resets the string "letterFound"
-        restartGame() // Restarts the game. 
+        restartGame(); // Restarts the game. 
     }
 });
 
@@ -69,7 +69,7 @@ function addPhraseToDisplay(arr) {
                 letterLi.className = 'letter'; // If li includes a character - add the class "letter"
             } else {
                 letterLi.className = 'space'; // If li includes a space - add the class "space". (This was not mentioned in the instructions)
-            };
+            }
         ulList.appendChild(letterLi); // appends the li elements to the ul. 
     }
 }
@@ -104,14 +104,14 @@ document.addEventListener('click', (e) => {
         e.target.className = 'chosen';
         e.target.disabled = true;
             if (checkLetter(e.target.innerHTML)) {
-                letterFound += checkLetter(e.target.innerHTML) // Adds the input letter to a variable
+                letterFound += checkLetter(e.target.innerHTML); // Adds the input letter to a variable
             } else {
                 missed++; // Adds +1 to the variable missed
                 const targetHeart = 5 - missed; 
                 hearts[targetHeart].innerHTML = lostHeartString; // Removes the last heart from the site and replaces the heart with a empty heart
             }
 }
-checkWin() // A function that checks the result. 
+checkWin(); // A function that checks the result. 
 });
 
 
@@ -123,7 +123,7 @@ function checkWin() {
        if (show.length >= 1 && show.length === letter.length && phrases.length >= 2){
             toggleFinalOverlay('win', 'Congratulations!', 'Continue Game'); // Toggles an overlay when a player got all the characters correct.
         } else if (show.length >= 1 && show.length === letter.length && phrases.length === 1) {
-            toggleFinalOverlay('win', 'Congratulations, you completed the game!', 'Restart Game') // Toggles an overlay when a player gets all phrases correct.
+            toggleFinalOverlay('win', 'Congratulations, you completed the game!', 'Restart Game'); // Toggles an overlay when a player gets all phrases correct.
         } else if (missed >= 5) {
             toggleFinalOverlay('lose', "Sorry, wrong guess!", 'Restart Game'); // Toggles an overlay when a player has lost all five hearts. 
         }
@@ -142,12 +142,12 @@ function toggleFinalOverlay(overlayClass, titleText, btnText){
  // A function to continue the game and get a new array
  function continuePlaying(){
     missed = 0; // Resets the missed variable
-    resetHearts() // Resets the hearts
-    resetButtons() // Resets the buttons
-    clearLetterShowClass() // Removes the "show" class from the li elements
-    removeOldPhraseFromUl() // Removes the completed phrase from the ul
-    removeStringFromPhrases() // Removes the completed phrase from the array
-    getNewArrayAndAppendToSite() // Gets a new array, create li elements and appends to the ul. 
+    resetHearts(); // Resets the hearts
+    resetButtons(); // Resets the buttons
+    clearLetterShowClass(); // Removes the "show" class from the li elements
+    removeOldPhraseFromUl(); // Removes the completed phrase from the ul
+    removeStringFromPhrases(); // Removes the completed phrase from the array
+    getNewArrayAndAppendToSite(); // Gets a new array, create li elements and appends to the ul. 
 }
 
 // Removes a string from the array. Used when a player completes a phrase. 
@@ -159,12 +159,12 @@ function toggleFinalOverlay(overlayClass, titleText, btnText){
 // A function to restart the game
 function restartGame(){
     missed = 0; // Resets the missed variable
-    resetHearts() // Resets the hearts
-    resetButtons() // Resets the buttons
-    clearLetterShowClass() // Removes the "show" class from the li elements
-    removeOldPhraseFromUl() // Removes the completed phrase from the ul
-    addRemovedPhrasesToArray() // If the player has completed any phrases before loosing all hearts this function adds the phrases back to the array to restart the game woth all phrases. 
-    getNewArrayAndAppendToSite() // Gets a new array, create li elements and appends to the ul. 
+    resetHearts(); // Resets the hearts
+    resetButtons(); // Resets the buttons
+    clearLetterShowClass(); // Removes the "show" class from the li elements
+    removeOldPhraseFromUl(); // Removes the completed phrase from the ul
+    addRemovedPhrasesToArray(); // If the player has completed any phrases before loosing all hearts this function adds the phrases back to the array to restart the game woth all phrases. 
+    getNewArrayAndAppendToSite(); // Gets a new array, create li elements and appends to the ul. 
 }
 
 // Resets the lost hearts
@@ -202,7 +202,7 @@ function removeOldPhraseFromUl() {
 function addRemovedPhrasesToArray() {
     if (phrases.length === 1) {
         while ( removedPhrases.length > 0 ) {
-            phrases.push(removedPhrases[0])  // Adds the phrases to the phrases array
+            phrases.push(removedPhrases[0]);  // Adds the phrases to the phrases array
             removedPhrases.splice(0, 1); // Removes the phrase from the removedPhrases array
         }
     }
